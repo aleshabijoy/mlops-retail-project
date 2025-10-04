5c535dff29d0440c029798cf8c6ccd099f1ded80
aleshabijoy <165121698+aleshabijoy@users.noreply.github.com>
2025-10-04 16:48:20 +0530

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,8 +1,10 @@
+import os
+os.environ["MLFLOW_TRACKING_URI"] = "file:///tmp/mlruns"
 import mlflow
 mlflow.set_tracking_uri("file:///tmp/mlruns")
 
 from fastapi.testclient import TestClient
-from main import app 
+from main import app
 from unittest.mock import patch
 
 client = TestClient(app)
@@ -13,7 +15,8 @@ def test_read_root():
     assert response.json() == {"message": "Welcome to the Retail Analytics API. Go to /docs for endpoints."}
 
 @patch('main.mlflow')
-def test_trigger_training_endpoint(mock_mlflow):
+@patch('analysis_logic.mlflow')
+def test_trigger_training_endpoint(mock_mlflow_main, mock_mlflow_analysis):
     response = client.post("/trigger-training")
     assert response.status_code == 200
     response_data = response.json()
@@ -21,5 +24,6 @@ def test_trigger_training_endpoint(mock_mlflow):
     assert "run_id" in response_data
     assert "mse" in response_data
     assert response_data["message"] == "Model training complete."
-    mock_mlflow.set_experiment.assert_called_once_with("Retail Spending Predictor")
-    mock_mlflow.log_metric.assert_called_once()
+    mock_mlflow_main.set_experiment.assert_called_once_with("Retail Spending Predictor")
+    mock_mlflow_main.log_metric.assert_called_once()
+    
